fc8f26272bc06747a9239058ad2e2104934ac527
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-12 19:44:30 -0400

    hello world

--- helloworld.py
+++ helloworld.py
@@ -8,3 +8,8 @@ def how_much_money_do_i_have(money):
     return f"I've got {money} dollars!"
 
 print (how_much_money_do_i_have(1553672))
+
+def name_and_amount(name, amount):
+    return f"{name} is {amount} times richer than us!"
+
+print(name_and_amount("Jeff Bezos", 14489271073))
