d290e56b3259d70a35b7bca3ebbd021f81fa92d2
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-06-12 19:02:53 -0400

    python

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
-print("Hello World")
+# print("Hello World")
+def greet(name):
+    return f"hello, {name}!"
+
+print(greet("folks"))
+
+print (greet("world"))
+
+def how_much_money_do_i_have(money):
+    return f"I've got {money} dollars!"
+
+print (how_much_money_do_i_have(1553672))
