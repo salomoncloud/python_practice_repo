f3e66fb56bb4a4f2ef0a1a07d28812513d871a35
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-12 20:06:26 -0400

    hello world

--- helloworld.py
+++ helloworld.py
@@ -13,3 +13,11 @@ def name_and_amount(name, amount):
     return f"{name} is {amount} times richer than us!"
 
 print(name_and_amount("Jeff Bezos", 14489271073))
+
+def calculate_sum(numbers):
+    return sum(numbers)
+input_numbers = input("Enter numbers separated by spaces: ")
+
+numbers_list = list(map(int, input_numbers.split()))
+total_sum = calculate_sum(numbers_list)
+print("The sum of the numbers is:", total_sum)
