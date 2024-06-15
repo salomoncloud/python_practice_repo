d442eaecbf4efc3b4012b2a597a18d0836a31bca
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-15 11:44:10 -0400

    math fucntions

---
+++ datatypes.py
@@ -0,0 +1,5 @@
+# fundamental data types - int float bool str list tuple set dict --- data types are just values, stored info that can be used
+# classes - custom types of data 
+# specialized data types - special modules 
+# none - means nothing, zero, no value
+# gonna practice math functions here
