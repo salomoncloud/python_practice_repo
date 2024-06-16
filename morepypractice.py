cdb56f523e5b1ee529d9c269934fd2836dc58526
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-16 15:33:04 -0400

    practice

---
+++ morepypractice.py
@@ -0,0 +1,10 @@
+# expressions vs statements
+iq = 100
+user_age = iq/5
+#the expression is the iq/5, the part of the code that produces a value, the statement is the entire line for example line 3 entirely
+
+# augmented assignment operator
+random_value = 5
+random_value += 2
+
+print(random_value)
