aee109a398369ed1f0a86acea7e6f5d535718ae1
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-16 15:34:05 -0400

    py

--- morepypractice.py
+++ morepypractice.py
@@ -1,6 +1,7 @@
 # expressions vs statements
 iq = 100
 user_age = iq/5
+print(user_age)
 #the expression is the iq/5, the part of the code that produces a value, the statement is the entire line for example line 3 entirely
 
 # augmented assignment operator
