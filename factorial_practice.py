24cd0157c461da648f7e1d5a3112200948030abf
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-17 18:50:09 -0400

    factorial

--- factorial_practice.py
+++ factorial_practice.py
@@ -2,9 +2,10 @@ def factorial(n):
     # n is the parameter of the function, which will take the value for which we want to calculate the factorial
     if n == 0:
         return 1
+    # stops the recursion from continuing indefinitely
     else:
         return n * factorial(n - 1)
-
-# calculate factorial
+# multiplied by the result of calling the factorial function
 result = factorial(3)
+# starts the recursion process to calculate the factorial
 print(result)
