c91c93a44ad176a8a4d2610142187e4c2673ba99
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-17 18:46:35 -0400

    factorial

--- factorial_practice.py
+++ factorial_practice.py
@@ -1,9 +1,10 @@
 def factorial(n):
+    # n is the parameter of the function, which will take the value for which we want to calculate the factorial
     if n == 0:
         return 1
     else:
         return n * factorial(n - 1)
 
-# Calculate the factorial of 10
+# calculate factorial
 result = factorial(10)
 print(result)
