d1686d048342dc06f7eba211b023aa13b9cf0012
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-17 20:27:19 -0400

    mathhhhhhh

--- more_challenges.py
+++ more_challenges.py
@@ -14,5 +14,40 @@ if is_prime(num):
 else:
     print(f"{num} is not a prime number.")
 
-    #
-    
+    # fibonacci calculations
+
+
+# display the Fibonacci sequence up to defined term
+
+# Prompt user to input the number of terms they want in the Fibonacci sequence
+nterms = int(input("How many terms? "))
+
+# first two terms of the Fibonacci sequence
+n1, n2 = 0, 1
+
+# counter to keep track of the number of terms generated
+count = 0
+
+# check if the number of terms is valid (must be a positive integer)
+if nterms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return the first term (n1)
+elif nterms == 1:
+   print("Fibonacci sequence up to", nterms, ":")
+   print(n1)
+# generate Fibonacci sequence for n terms
+else:
+   print("Fibonacci sequence:")
+   # loop to generate the Fibonacci sequence
+   while count < nterms:
+       # print the current term in the sequence
+       print(n1)
+       # calculate the next term in the sequence
+       nth = n1 + n2
+       # update the values of n1 and n2 to the next terms in the sequence
+       n1 = n2
+       n2 = nth
+       # increment the counter by 1
+       count += 1
+
+
