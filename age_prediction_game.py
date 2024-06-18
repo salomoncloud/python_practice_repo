8a0fc6e929ee54c1d999ad3b93baef23069aa445
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-17 23:34:49 -0400

    game

--- age_prediction_game.py
+++ age_prediction_game.py
@@ -32,3 +32,22 @@ while balance > 0:
                 print("You've used all your guesses. Charging a $25 penalty.")
                 balance -= 25
                 # this is what happens when you bet, you lose money!! dont do it! anyways, here is the penalty in game
+
+            if guess != random_int:
+            print("Sorry, you didn't guess the number. Deducting $50.")
+            balance -= 50
+
+                # Display the player's current balance
+        print(f"Your current balance is: ${balance}")
+        
+        if balance <= 0:
+            print("Your broke!")
+            choice = input("Do you want to add more money or do the smart thing and save your money for rainy days? Type 'im an idiot' to add more money or 'quit' to quit: ").strip().lower()
+
+        if choice == 'add':
+                additional_money = float(input("Enter your bet!: $"))
+                balance += additional_money
+
+        elif choice == 'quit':
+                print("Smart move, go home.")
+                break
