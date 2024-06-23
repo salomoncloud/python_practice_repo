f9575becbf52c3574221092c3e8a1d13de9958da
salomoncloud <salomon.cloud.dev@gmail.com>
2024-06-23 14:46:33 -0400

    practice

--- practical_notes.py
+++ practical_notes.py
@@ -3,3 +3,14 @@
 weather = "It's \"kind of\" sunny"
 
 print(weather)
+
+# formatted strings
+# example
+name = 'salomon'
+age = 26
+print("hi " + name + ". Are you " + str(age) + " years old?")
+# a better way to do this^ is :
+name_for_example = 'fritz'
+age_for_example = 27
+print(f'hi {name_for_example}. You are {age_for_example} years old.') 
+# that f helps us make the code cleaner
