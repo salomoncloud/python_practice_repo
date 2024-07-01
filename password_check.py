22c653228cf9e4a7ef72f56ec9361bf3c8782e58
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-01 11:43:56 -0400

    password challenge

--- password_check.py
+++ password_check.py
@@ -1,4 +1,5 @@
 username = input("username: ")
 password = input("password: ")
-passwordlength = print(len(password))
-print(f"{username}, your password {print("*" * password)} is {passwordlength} letters long")
+password_length = len(password)
+hidden_password = "*" * password_length
+print(f"{username}, your password {hidden_password} is {password_length} letters long")
