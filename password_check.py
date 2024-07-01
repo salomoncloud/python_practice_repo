759b66d86edf0c4b0a3fff13c8ab5a3374bef6a8
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-01 11:45:18 -0400

    challenge

--- password_check.py
+++ password_check.py
@@ -1,5 +1,5 @@
 username = input("username: ")
 password = input("password: ")
 password_length = len(password)
-hidden_password = "*" * password_length
+hidden_password = "*" * password_length # here you take the length of your password and convert it all to stars
 print(f"{username}, your password {hidden_password} is {password_length} letters long")
