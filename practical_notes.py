f9d00b2cdbb8c736403670c785230d7c64cf46e3
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-01 15:16:40 -0400

    practice

--- practical_notes.py
+++ practical_notes.py
@@ -14,3 +14,22 @@ name_for_example = 'fritz'
 age_for_example = 27
 print(f'hi {name_for_example}. You are {age_for_example} years old.') 
 # that f helps us make the code cleaner
+
+# lists - ordered sequence of objects of any type
+fruits = ['apples', 
+          'oranges', 
+          'bananas', 
+          'grapes', 
+          'peaches']
+
+veggies = ['brocoli', 
+           'tomatoes', 
+           'celery', 
+           'salad', 
+           'eggplant']
+
+# lists are data structures - data structures are a way to organize info together
+#print(fruits[2]) would print out bananas
+
+# list slicing - similar to string slicing with the stop start and stepover
+# print(fruits[0::2]) this would skip over every second one
