569094b21995093ef445db6bcaa3484b88cb107e
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-01 11:19:46 -0400

    challenge

--- another_age_guessing_practice.py
+++ another_age_guessing_practice.py
@@ -1,10 +1,3 @@
-# defining the for calculating the age, the function takes day
-def calculate_age(day, month, year):
-    # we are getting the current date using the today()
-    today = date.today()
-    # convering year, month and day into birthdate
-    birthdate = date(year, month, day)
-    # calculating the age 
-    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-    # return the age value
-    return age
+birth_year = input ('what year were you born in?')
+age = 2024 - int(birth_year)
+print(f"your age is: {age}")
