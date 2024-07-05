b4aaa475f8b8525cd9658a95912f4a3f46da3836
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-05 14:49:43 -0400

    matrix

---
+++ matrix_and_methods.py
@@ -0,0 +1,11 @@
+# matrix is a way to describe 2D lists - multi-dimensional lists
+# ex
+matrix = [
+    [1,2,3],
+    [2,4,6],
+    [7,8,9]
+]
+# in this example we have the main array which is the first pair of square brackets, and than the 2nd containing the numbers
+# matrix's are used for ml models and image processing, and they work with 1's and 0's
+# to access things in matrix, ex:
+print(matrix[0][1])
