08553020194b33c90d98c7334070de8866ec86ea
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-06 19:51:53 -0400

    dict

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -4,7 +4,7 @@ dictionary = {
     'positive_kd': False
 }
 
-# print(dictionary['player name']) will print star_destroyer
+print(dictionary['player name']) # will print star_destroyer
 
 
 # for the key values in dictionary, you can use strings, boolean, and numbers
@@ -13,3 +13,10 @@ dictionary = {
 # dictionary methods - a good way to access a key to see if it exists is to use .get
 # ex
 # print(dictionary.get('power')) this will return None
+# you can also do print(dictionary.get('power', 100)) - by adding the 100, your adding a default value
+# ex- if power level is not found, it will make 100 the power level by default, but if power is assigned already, it will use og
+
+# another way of creating a dictionary ----
+
+players = dict(name='young_vader25')
+print(players)
