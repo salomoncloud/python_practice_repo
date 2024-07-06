4c9a9492ff4bdf8ede98f656d2fa89daf8cedc7d
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-06 19:45:07 -0400

    dict methods

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -4,6 +4,12 @@ dictionary = {
     'positive_kd': False
 }
 
-print(dictionary['player name'])
+# print(dictionary['player name']) will print star_destroyer
+
+
 # for the key values in dictionary, you can use strings, boolean, and numbers
 # key needs to be immutable, so it cant change, thats why we cant use lists for key value
+
+# dictionary methods - a good way to access a key to see if it exists is to use .get
+# ex
+# print(dictionary.get('power')) this will return None
