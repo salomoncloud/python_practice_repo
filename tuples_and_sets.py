3e8fa7a3e568e852bc85912edbaa8f07e600a20a
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-06 20:21:25 -0400

    tup

--- tuples_and_sets.py
+++ tuples_and_sets.py
@@ -4,3 +4,12 @@
 # faster than lists
 # ex - uber using tuple for long/lat of location
 tuple_list = [(1, 2), (3, 4), (5, 6)]
+
+#new_tuple = tuple_list[1:3]
+# will print our 3,4 and 5,6
+#print(new_tuple)
+
+x = tuple_list[0]
+y = tuple_list[1]
+print(x) # x will equal to 1,2
+print(y) # y will equal to 3,4
