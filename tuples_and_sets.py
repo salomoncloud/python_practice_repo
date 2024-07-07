cf840404288147a72c0f015d0c98e11f14203a0d
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-06 20:55:29 -0400

    tup and set

--- tuples_and_sets.py
+++ tuples_and_sets.py
@@ -39,3 +39,21 @@ print(my_set) # will not return duplicates, everything is unique
 
 # to make a list into a set and remove duplicates from that list, simply do set(list) method
 # could be useful when a website has email addresses that may be duplicate, you could convert that list into set to not spam email over and over
+
+#set methods
+set_of_nums = {5,10,15,20,25,30}
+second_set_of_nums = {30,35,40,45,50,55}
+
+print(set_of_nums.difference(second_set_of_nums)) # will find the differences between sets
+
+#print(set_of_nums.discard(5))
+#print(set_of_nums) these will remove 5 from the set
+
+#print(set_of_nums.difference_update(second_set_of_nums))
+#print(set_of_nums) differences are removed between the 2 sets
+
+print(set_of_nums.intersection(second_set_of_nums)) #will print the common things between 2 sets, in this case 30
+
+print(set_of_nums.isdisjoint(second_set_of_nums)) # asks if they are overlapping, nothing in common, because of 30 it will be false
+
+print(set_of_nums.union(second_set_of_nums)) # will join both sets and removed duplicates
