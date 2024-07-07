0e52c9bf729894c32ac824e50815e90ea0783efe
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-06 20:18:06 -0400

    tuple

---
+++ tuples_and_sets.py
@@ -0,0 +1,6 @@
+# tuples are like lists but immutable 
+# benefits of tuple are efficient and safer code because its predictable and people wont modify it
+# however tuple is less flexible
+# faster than lists
+# ex - uber using tuple for long/lat of location
+tuple_list = [(1, 2), (3, 4), (5, 6)]
