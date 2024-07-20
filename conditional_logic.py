4e65a619f827aa7c943dd13908c1ef8943208a94
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-20 09:55:53 -0400

    practice

--- conditional_logic.py
+++ conditional_logic.py
@@ -1,7 +1,7 @@
 # booleans
 # creating something that checks your age and license before starting a car, for example
 is_old = True
-is_licenced = True
+is_licenced = False
 
 if is_old and is_licenced:
     print("you are old enough to drive this car")
