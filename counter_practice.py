9d3ba2f28cc58bbf1e072fe11496950092264dfb
salomoncloud <salomon.cloud.dev@gmail.com>
2024-07-31 16:33:04 -0400

    count

---
+++ counter_practice.py
@@ -0,0 +1,9 @@
+# using looping, im gonna practice here a loop over an iterable list and sum up the total of the list
+
+waiting_list = [1,2,3,4,5,6,7,8,9,10]
+
+counter = 0
+for item in waiting_list:
+    counter = counter + item
+
+print(counter)
