7717d5d7a01a78443d8b3e88250b147bdfa68e95
Salomon <158000174+salomoncloud@users.noreply.github.com>
2025-07-15 09:11:38 -0400

    Update countdown_to_whatever.py

--- countdown_to_whatever.py
+++ countdown_to_whatever.py
@@ -4,7 +4,12 @@ def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
+        print(timer, end='\r')  # Overwrite the line each second
         time.sleep(1)
         t -= 1
-    print("boom!")
+
+    print("run")
+
+t = input("Enter the time in seconds: ")
+
+countdown(int(t))
